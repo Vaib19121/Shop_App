f4d50e718fc3592209dcb5602313b4651c3b9bb7
Vaibhav Mehar <vaibhavmehar16@gmail.com>
2021-09-13 11:10:08 +0530

    added ionicons in headerbutton.js

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Platform } from "react-native";
 import { HeaderButton } from 'react-navigation-header-buttons';
 import { Ionicons } from '@expo/vextor-icons';
 
 import Color from "../../constants/Color";
 
 const CustomHeaderButton = props => {
-    return <HeaderButton {...props} IconComponent={} />;
+    return <HeaderButton {...props} IconComponent={Ionicons} iconSize={23} color={Platform.OS === "android" ? "white" : Color.primary} />;
 };
+
+export default CustomHeaderButton; 
